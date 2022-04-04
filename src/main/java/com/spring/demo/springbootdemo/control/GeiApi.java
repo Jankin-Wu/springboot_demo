d95e6113bf2b5af39d85bcfee25d3532772dcdfc
Jankin-Wu <63941607+Jankin-Wu@users.noreply.github.com>
2022-04-04 18:21:16 +0800

    修改返回提示

--- src/main/java/com/spring/demo/springbootdemo/control/GeiApi.java
+++ src/main/java/com/spring/demo/springbootdemo/control/GeiApi.java
@@ -5,12 +5,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping()
 public class GeiApi {
 
     @GetMapping("get")
     @ResponseBody
     public String getInfo() {
-        return "测试在别的分支上push是否会触发构建";
+        return "如果输出这行字则证明成功运行";
     }
 }
