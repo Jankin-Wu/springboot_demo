15b5eb0b7e56d6ab541590ff623123259e652c99
Jankin-Wu <63941607+Jankin-Wu@users.noreply.github.com>
2022-04-04 18:09:29 +0800

    打包方式修改为war包

--- src/main/java/com/spring/demo/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/spring/demo/springbootdemo/SpringbootDemoApplication.java
@@ -2,14 +2,18 @@ package com.spring.demo.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootDemoApplication {
+public class SpringbootDemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemoApplication.class, args);
     }
-@Override
+
+    @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(SpringbootDemoApplication.class);
     }
