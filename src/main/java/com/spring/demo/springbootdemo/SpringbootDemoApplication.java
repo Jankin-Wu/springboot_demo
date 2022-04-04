f355e2e5aff47210eebaf25db54f8929df9b71fa
Jankin-Wu <63941607+Jankin-Wu@users.noreply.github.com>
2022-04-04 18:17:40 +0800

    jar包版本

--- src/main/java/com/spring/demo/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/spring/demo/springbootdemo/SpringbootDemoApplication.java
@@ -2,21 +2,12 @@ package com.spring.demo.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootDemoApplication extends SpringBootServletInitializer {
+public class SpringbootDemoApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemoApplication.class, args);
     }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(SpringbootDemoApplication.class);
-    }
-
 
 }
