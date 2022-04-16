8caf6036e74ebf3970aac8c0508ba1ac639af82a
Jankin-Wu <63941607+Jankin-Wu@users.noreply.github.com>
2022-04-16 15:39:16 +0800

    添加Jenkinsfile文件

--- src/main/java/com/spring/demo/springbootdemo/controller/GetApi.java
+++ src/main/java/com/spring/demo/springbootdemo/controller/GetApi.java
@@ -1,15 +1,15 @@
-package com.spring.demo.springbootdemo.control;
+package com.spring.demo.springbootdemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GeiApi {
+public class GetApi {
 
     @GetMapping("get")
     @ResponseBody
     public String getInfo() {
-        return "如果输出这行字则证明成功运行";
+        return "如果输出这行字则证明成功使用Jenkinsfile构建";
     }
 }
